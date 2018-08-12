e48e327838440caede627507b37abf448edbbbd5
Tom Lawlor <walot111@live.com.au>
2018-08-13 02:10:42 +1000

    Made graph bigger and fixed errors

--- graph.py
+++ graph.py
@@ -4,43 +4,40 @@ app = Flask(name)
 DATABASE = 'sensehat.db'
 
 def get_db():
-db = getattr(g,'_database',None)
-if db is None:
-     db = g._database = sqlite3.connect[DATABASE]
-	 return db
+	db = getattr(g,'_database',None)
+	if db is None:
+		db = g._database = sqlite3.connect[DATABASE]
+		return db
 	 
 @app.teardown_appcontext
 def close_connection (exception)
-db = getattr(g, '_database",None)
-if db is not None
-db.close()
+	db = getattr(g, '_database",None)
+	if db is not None
+		db.close()
 
 def query_db(query, args = (), one = false):
-cur = get_db().execute (query, args)
-rv = cur.fetchall()
-cur.close
-return (rb[0] if rv else None) if one else rv
-
-
-
-
+	cur = get_db().execute (query, args)
+	rv = cur.fetchall()
+	cur.close
+	return (rb[0] if rv else None) if one else rv
 
 @app.route("/")
-def hello():
-for sense in query_db ('SELECT * FROM Senesehat_data'):
-timestamp.append(sense ['timestamp'])
-timestamp.append (sense ['temp'] )
-timestamp.append (sense ['hum'] )
+def index():
+	timestamp,temp,hum = []
+	for sense in query_db ('SELECT * FROM Senesehat_data'):
+		timestamp.append(sense ['timestamp'])
+		timestamp.append (sense ['temp'] )
+		timestamp.append (sense ['hum'] )
 
-templateData = (
-'timestamp : timestamp,
-'temp' : temp,
-'hum' : hum
-)
-return render_templates ('Demo_graph.html', **templateData)
+	templateData = (
+	'timestamp' : timestamp,
+	'temp' : temp,
+	'hum' : hum
+	)
+	return render_templates ('Demo_graph.html', **templateData)
 
 if name == "main":
-app.run(host='0.0.0.0', port=80 debug=True)
+	app.run(host='0.0.0.0', port=80 debug=True)
 
 
 
